f6a6d0a9ab2ecec97c232671b0b54cd506145bb6
AlienQueen <goupilpierre@gmail.com>
2011-08-16 15:22:19 +0200

    GFX

--- src/main/java/org/alienlabs/hatchetharry/view/HomePage.java
+++ src/main/java/org/alienlabs/hatchetharry/view/HomePage.java
@@ -87,7 +87,7 @@ public class HomePage extends TestReportPage implements AtmosphereResourceEventL
 		this.addHeadResources();
 
 		// Welcome message
-		this.add(new Label("message", "version 0.0.2 built on Sunday, 14th of August 2011"));
+		this.add(new Label("message", "version 0.0.2 built on Tuesday, 16th of August 2011"));
 
 		// Hand
 		this.buildHand();
