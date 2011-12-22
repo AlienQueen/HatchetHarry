23c73514e0d202571be4a315542c769c4085468f
AlienQueen <goupilpierre@gmail.com>
2011-12-22 22:59:12 +0100

    DONE: -PersistenceService instances are transient fields, now   TODO: -update DataBox when joining a game

--- src/main/java/org/alienlabs/hatchetharry/service/DataGenerator.java
+++ src/main/java/org/alienlabs/hatchetharry/service/DataGenerator.java
@@ -74,7 +74,7 @@ public class DataGenerator implements InitializingBean
 	@SpringBean
 	private MagicCardDao magicCardDao;
 	@SpringBean
-	private PersistenceService persistenceService;
+	private transient PersistenceService persistenceService;
 
 	@Required
 	public void setDeckDao(final DeckDao _deckDao)
