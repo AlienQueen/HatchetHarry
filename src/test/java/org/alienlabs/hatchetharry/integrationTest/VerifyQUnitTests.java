6f014c2613ca9e2717fc7560a8ae0c2941885d6e
AlienQueen@sulaco <goupilpierre@gmail.com>
2013-01-16 22:35:20 +0100

    Fixed POM in order to suppress com.google.collections/google-collections because it used to provoke classpath problems + DB setup at launch time activated

--- src/test/java/org/alienlabs/hatchetharry/integrationTest/VerifyQUnitTests.java
+++ src/test/java/org/alienlabs/hatchetharry/integrationTest/VerifyQUnitTests.java
@@ -1,48 +1,57 @@
 package org.alienlabs.hatchetharry.integrationTest;
 
+import org.alienlabs.hatchetharry.view.component.CardRotateBehavior;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-//public class VerifyQUnitTests extends SeleneseTestCase
-//{
-//	Selenium selenium;
-//
-//	@Override
-//	@Before
-//	public void setUp()
-//	{
-//		try
-//		{
-//			super.setUp();
-//			this.selenium = new DefaultSelenium("localhost", 4444, "*firefox",
-//					"http://localhost:8080/");
-//			this.selenium.start();
-//		}
-//		catch (final Exception e)
-//		{
-//			e.printStackTrace();
-//		}
-//	}
-//
-//	@Test
-//	public void testQunit()
-//	{
-//		this.selenium.open("/");
-//		this.verifyTrue(this.selenium.isTextPresent("8 passed, 0 failed"));
-//	}
-//
-//	@Override
-//	@After
-//	public void tearDown()
-//	{
-//		try
-//		{
-//			super.tearDown();
-//		}
-//		catch (final Exception e)
-//		{
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		this.selenium.stop();
-//	}
-//
-// }
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.SeleneseTestCase;
+
+
+public class VerifyQUnitTests extends SeleneseTestCase
+{
+	private static final Logger LOGGER = LoggerFactory.getLogger(CardRotateBehavior.class);
+
+	@Override
+	@Before
+	public void setUp()
+	{
+		try
+		{
+			super.setUp();
+			this.selenium = new DefaultSelenium("localhost", 4444, "*firefox",
+					"http://localhost:8080/");
+			this.selenium.start();
+		}
+		catch (final Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testQunit()
+	{
+		this.selenium.open("/");
+		this.verifyTrue(this.selenium.isTextPresent("8 passed, 0 failed"));
+	}
+
+	@Override
+	@After
+	public void tearDown()
+	{
+		try
+		{
+			super.tearDown();
+		}
+		catch (final Exception e)
+		{
+			VerifyQUnitTests.LOGGER.error("error in VerifyQUnitTests#tearDown()!", e);
+		}
+		this.selenium.stop();
+	}
+
+}
