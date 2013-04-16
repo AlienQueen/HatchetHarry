094b4b5e7f9a8a412cb04c7c6bc6fc2d4a6646f4
AlienQueen@sulaco <goupilpierre@gmail.com>
2013-04-16 04:27:14 +0200

    FIX for second part of HAHA-48 => closed

--- src/main/java/org/alienlabs/hatchetharry/view/page/HomePage.java
+++ src/main/java/org/alienlabs/hatchetharry/view/page/HomePage.java
@@ -1150,8 +1150,10 @@ public class HomePage extends TestReportPage
 	@Subscribe
 	public void updateTime(final AjaxRequestTarget target, final Date event)
 	{
+		target.prependJavaScript("var chatPos = document.getElementById('chat').scrollTop; ");
 		this.clockPanel.setDefaultModelObject(event.toString());
 		target.add(this.clockPanel);
+		target.appendJavaScript("document.getElementById('chat').scrollTop = chatPos; ");
 	}
 
 	@Subscribe
