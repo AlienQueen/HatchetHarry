5a5cea31fc5223cd80437ab8bd9af3af08865633
AlienQueen@sulaco <goupilpierre@gmail.com>
2013-04-21 18:52:21 +0200

    DB schema should be OK + FIXED Selenium test

--- src/test/java/org/alienlabs/hatchetharry/integrationTest/VerifyClientSideTests.java
+++ src/test/java/org/alienlabs/hatchetharry/integrationTest/VerifyClientSideTests.java
@@ -56,7 +56,7 @@ public class VerifyClientSideTests
 			+ "	if (elementInViewport(elementToLookFor)) {\n"
 			+ "		break;\n"
 			+ "	} else {\n"
-			+ "		window.scrollBy(0,100);\n}\n}";
+			+ "		window.scrollBy(50,150);\n}\n}";
 
 	@BeforeClass
 	public static void setUpClass()
