0fda61bfda3853874ae350572d06905971d1177c
AlienQueen <goupilpierre@gmail.com>
2013-08-11 22:46:26 +0200

    FindBugs

--- src/main/java/org/alienlabs/hatchetharry/model/Counter.java
+++ src/main/java/org/alienlabs/hatchetharry/model/Counter.java
@@ -17,7 +17,7 @@ import org.hibernate.annotations.CascadeType;
 
 @Entity
 @Table(name = "Counter")
-public class Counter implements Serializable, Comparable
+public class Counter implements Serializable, Comparable<Counter>
 {
 	private static final long serialVersionUID = 1L;
 
@@ -88,10 +88,121 @@ public class Counter implements Serializable, Comparable
 		this.token = _token;
 	}
 
+	/**
+	 * This class defines a compareTo(...) method and doesn't inherit its
+	 * equals() method from java.lang.Object. Generally, the value of compareTo
+	 * should return zero if and only if equals returns true. If this is
+	 * violated, weird and unpredictable failures will occur in classes such as
+	 * PriorityQueue. In Java 5 the PriorityQueue.remove method uses the
+	 * compareTo method, while in Java 6 it uses the equals method. From the
+	 * JavaDoc for the compareTo method in the Comparable interface: It is
+	 * strongly recommended, but not strictly required that (x.compareTo(y)==0)
+	 * == (x.equals(y)). Generally speaking, any class that implements the
+	 * Comparable interface and violates this condition should clearly indicate
+	 * this fact. The recommended language is
+	 * "Note: this class has a natural ordering that is inconsistent with equals."
+	 */
 	@Override
-	public int compareTo(final Object o)
+	public int compareTo(final Counter c)
 	{
-		return this.getCounterName().compareTo(((Counter)o).getCounterName());
+		if (this.equals(c))
+		{
+			return 0;
+		}
+
+		if (this.getCounterName().equals(c.getCounterName()))
+		{
+			return -1;
+		}
+		return this.getCounterName().compareTo(c.getCounterName());
 	}
-	
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = (prime * result) + ((this.card == null) ? 0 : this.card.hashCode());
+		result = (prime * result) + ((this.counterId == null) ? 0 : this.counterId.hashCode());
+		result = (prime * result) + ((this.counterName == null) ? 0 : this.counterName.hashCode());
+		result = (prime * result)
+				+ ((this.numberOfCounters == null) ? 0 : this.numberOfCounters.hashCode());
+		result = (prime * result) + ((this.token == null) ? 0 : this.token.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj)
+	{
+		if (this == obj)
+		{
+			return true;
+		}
+		if (obj == null)
+		{
+			return false;
+		}
+		if (this.getClass() != obj.getClass())
+		{
+			return false;
+		}
+		final Counter other = (Counter)obj;
+		if (this.card == null)
+		{
+			if (other.card != null)
+			{
+				return false;
+			}
+		}
+		else if (!this.card.equals(other.card))
+		{
+			return false;
+		}
+		if (this.counterId == null)
+		{
+			if (other.counterId != null)
+			{
+				return false;
+			}
+		}
+		else if (!this.counterId.equals(other.counterId))
+		{
+			return false;
+		}
+		if (this.counterName == null)
+		{
+			if (other.counterName != null)
+			{
+				return false;
+			}
+		}
+		else if (!this.counterName.equals(other.counterName))
+		{
+			return false;
+		}
+		if (this.numberOfCounters == null)
+		{
+			if (other.numberOfCounters != null)
+			{
+				return false;
+			}
+		}
+		else if (!this.numberOfCounters.equals(other.numberOfCounters))
+		{
+			return false;
+		}
+		if (this.token == null)
+		{
+			if (other.token != null)
+			{
+				return false;
+			}
+		}
+		else if (!this.token.equals(other.token))
+		{
+			return false;
+		}
+		return true;
+	}
+
 }
