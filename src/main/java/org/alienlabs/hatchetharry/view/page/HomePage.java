116128bd272f2b6c80b08bb8a5ff422c9d3c16fc
AlienQueen <goupilpierre@gmail.com>
2013-10-02 17:35:17 +0200

    FIXED context menu

--- src/main/java/org/alienlabs/hatchetharry/view/page/HomePage.java
+++ src/main/java/org/alienlabs/hatchetharry/view/page/HomePage.java
@@ -234,7 +234,7 @@ public class HomePage extends TestReportPage
 		// Welcome message
 		final Label message1 = new Label("message1",
 				"version 0.5.0 (release She said \"I love you\"),");
-		final Label message2 = new Label("message2", "built on Monday, 30th of September 2013.");
+		final Label message2 = new Label("message2", "built on Wednesday, 2nd of October 2013.");
 		this.add(message1, message2);
 
 		// Comet clock channel
