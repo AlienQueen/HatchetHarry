0615715ed276a93fca2bd10988e3af1c4b8377cc
AlienQueen <goupilpierre@gmail.com>
2013-12-23 14:52:33 +0100

    we now respect the scripts structure for visio

--- src/main/java/org/alienlabs/hatchetharry/view/page/HomePage.java
+++ src/main/java/org/alienlabs/hatchetharry/view/page/HomePage.java
@@ -1407,8 +1407,6 @@ public class HomePage extends TestReportPage
 				response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(
 						HomePage.class, "script/conference/webrtc4all.js")));
 				response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(
-						HomePage.class, "script/conference/initCall.js")));
-				response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(
 						HomePage.class, "script/qunitTests/codeUnderTest.js")));
 				response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(
 						HomePage.class, "script/qunitTests/HomePageTests.js")));
