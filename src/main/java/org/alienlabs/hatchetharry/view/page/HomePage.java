39ede5dccef22b9497bb725017956057bb72d37e
AlienQueen@sulaco <goupilpierre@gmail.com>
2013-12-24 17:30:42 +0100

    video-conference should be OK, now

--- src/main/java/org/alienlabs/hatchetharry/view/page/HomePage.java
+++ src/main/java/org/alienlabs/hatchetharry/view/page/HomePage.java
@@ -1402,10 +1402,10 @@ public class HomePage extends TestReportPage
 						HomePage.class, "script/dock/jquery.jqdock.min.js")));
 				response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(
 						HomePage.class, "script/qunitTests/qUnit-1.11.0-min.js")));
-				response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(
-						HomePage.class, "script/conference/SIPml-api.js")));
-				response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(
-						HomePage.class, "script/conference/webrtc4all.js")));
+				//response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(
+				//		HomePage.class, "script/conference/SIPml-api.js")));
+				//response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(
+				//		HomePage.class, "script/conference/webrtc4all.js")));
 				//response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(
 				//		HomePage.class, "script/conference/initConference.js")));
 				response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(
