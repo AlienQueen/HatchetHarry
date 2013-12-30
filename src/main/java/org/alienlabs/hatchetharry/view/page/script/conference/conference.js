f727dd93ce17f900cf90dd3c181282767f8aeac9
AlienQueen@derelict <goupilpierre@gmail.com>
2013-12-30 07:48:41 +0100

    changed visio

--- src/main/java/org/alienlabs/hatchetharry/view/page/script/conference/conference.js
+++ src/main/java/org/alienlabs/hatchetharry/view/page/script/conference/conference.js
@@ -27,20 +27,23 @@ var callAudio = function(id) {
 
 var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 peer.on('call', function(call) {
-  play('ringbacktone');
-  getUserMedia({video: false, audio: true}, function(stream) {
-    call.answer(stream); // Answer the call with an A/V stream.
-    call.on('stream', function(remoteStream) {
-      // Show stream in some video/canvas element.
-    	pause('ringbacktone');
-    });
-  }, function(err) {
+	// Answer the call automatically (instead of prompting user) for demo purposes
+	play('ringbacktone');
+      	call.answer(window.localStream);
+	call.on('stream', function(stream){
+        	jQuery('#their-video').prop('src', URL.createObjectURL(stream));
+      	});
+
+  };
+
+peer.on('error', function(err){
     pause('ringbacktone');
-    console.log('Failed to get local stream' ,err);
-  });
+    console.log('Failed to get local stream' ,err.message);
 });
 
 var hangup = function() {
+	pause('ringtone');
+	pause('ringbacktone');
 	peer.disconnect();
 	peer.destroy();
 }
