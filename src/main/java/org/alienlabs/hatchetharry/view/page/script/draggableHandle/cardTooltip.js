19e7ece0bb422a866bb1fc74a4ab0d5baec31f16
Alienµ <zpgoupil@zpgoupil2>
2014-02-18 17:56:46 +0100

    token tooltips?

--- src/main/java/org/alienlabs/hatchetharry/view/page/script/draggableHandle/cardTooltip.js
+++ src/main/java/org/alienlabs/hatchetharry/view/page/script/draggableHandle/cardTooltip.js
@@ -1,20 +1,18 @@
-jQuery(function() { 
-	window.setTimeout(function() {
-		if (typeof drawMode == 'undefined' || !drawMode) {
-			jQuery('.clickableCard').click(function(e) { 
-				jQuery('#cardTooltip${uuidValidForJs}').attr('style', 'display: block; position: absolute; left: ${posX}px; top: ' + (${posY} + 56) + 'px; z-index: 50;');
-				jQuery('#cardTooltip${uuidValidForJs} > span').attr('style', 'display: block;'); });
+window.setTimeout(function() {
+	if (typeof drawMode == 'undefined' || !drawMode) {
+		jQuery('.clickableCard').click(function(e) { 
+			jQuery('#cardTooltip${uuidValidForJs}').attr('style', 'display: block; position: absolute; left: ${posX}px; top: ' + (${posY} + 56) + 'px; z-index: 50;');
+			jQuery('#cardTooltip${uuidValidForJs} > span').attr('style', 'display: block;'); });
 
-			// For mobile
-			var hammertime${uuidValidForJs} = jQuery('.clickableCard').hammer();
-			hammertime${uuidValidForJs}.on('tap', function(ev) {
-				jQuery('#cardTooltip${uuidValidForJs}').attr('style', 'display: block; position: absolute; left: '
-						+ (${posX} + 127) + 'px; top: ' + (${posY} + 56)
-						+ 'px; z-index: 50;');
-				jQuery('#cardTooltip${uuidValidForJs} > span').attr('style', 'display: block;');
-			});
+		// For mobile
+		var hammertime${uuidValidForJs} = jQuery('.clickableCard').hammer();
+		hammertime${uuidValidForJs}.on('tap', function(ev) {
+			jQuery('#cardTooltip${uuidValidForJs}').attr('style', 'display: block; position: absolute; left: '
+					+ (${posX} + 127) + 'px; top: ' + (${posY} + 56)
+					+ 'px; z-index: 50;');
+			jQuery('#cardTooltip${uuidValidForJs} > span').attr('style', 'display: block;');
+		});
 
-			jQuery('#cardTooltip${uuidValidForJs}').hide(); 
-		}
-	}, 250);
-});
+		jQuery('#cardTooltip${uuidValidForJs}').hide(); 
+	}
+}, 500);
