2c995da7c76853e0a0880c9dae623251c3a5af57
AlienQueen <goupilpierre@gmail.com>
2014-02-27 05:05:21 +0100

    test mechanics

--- src/test/java/org/alienlabs/hatchetharry/view/component/CardPanelTest.java
+++ src/test/java/org/alienlabs/hatchetharry/view/component/CardPanelTest.java
@@ -1,12 +1,19 @@
 package org.alienlabs.hatchetharry.view.component;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 import org.alienlabs.hatchetharry.HatchetHarrySession;
 import org.alienlabs.hatchetharry.model.MagicCard;
+import org.alienlabs.hatchetharry.model.channel.PutToGraveyardCometChannel;
 import org.alienlabs.hatchetharry.serverSideTest.util.SpringContextLoaderBaseTest;
 import org.alienlabs.hatchetharry.service.PersistenceService;
 import org.alienlabs.hatchetharry.view.page.HomePage;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.atmosphere.EventBusMock;
+import org.apache.wicket.atmosphere.EventSubscription;
+import org.apache.wicket.atmosphere.PageKey;
+import org.apache.wicket.protocol.http.servlet.ServletWebRequest;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -14,7 +21,7 @@ import org.junit.Test;
 public class CardPanelTest extends SpringContextLoaderBaseTest
 {
 	@Test
-	public void testCardPanel()
+	public void testCardPanel() throws SecurityException, NoSuchMethodException
 	{
 		// Start a game and play a card
 		super.startAGameAndPlayACard(SpringContextLoaderBaseTest.tester,
@@ -38,9 +45,32 @@ public class CardPanelTest extends SpringContextLoaderBaseTest
 				.getComponentFromLastRenderedPage("parentPlaceholder:magicCards:1:cardPanel");
 		Assert.assertNotNull(card);
 
+		// Test Atmosphere
+		final HomePage homePage = (HomePage)SpringContextLoaderBaseTest.tester
+				.getLastRenderedPage();
+		final EventBusMock bus = (EventBusMock)(SpringContextLoaderBaseTest.webApp.getEventBus());
+		bus.fireRegistration(bus.getResource().uuid(), homePage);
+		bus.trackedPages.put(
+				bus.getResource().uuid(),
+				new PageKey(homePage.getPageId(), (((ServletWebRequest)homePage.getRequest())
+						.getContainerRequest().getRequestedSessionId())));
+		SpringContextLoaderBaseTest.webApp.resourceRegistered(bus.getResource().uuid(), homePage);
+		bus.registerPage((((ServletWebRequest)homePage.getRequest()).getContainerRequest()
+				.getRequestedSessionId()), homePage);
+		final Method c = HomePage.class.getDeclaredMethod("removeCardFromBattlefield",
+				AjaxRequestTarget.class, PutToGraveyardCometChannel.class);
+		c.setAccessible(true);
+		bus.register(homePage, new EventSubscription(homePage, null, c));
+
 		final PutToGraveyardFromBattlefieldBehavior ptgfbb = card
 				.getPutToGraveyardFromBattlefieldBehavior();
 		SpringContextLoaderBaseTest.tester.executeBehavior(ptgfbb);
+
+		SpringContextLoaderBaseTest.pageDocument = SpringContextLoaderBaseTest.tester
+				.getLastResponse().getDocument();
+		System.out.println("### " + SpringContextLoaderBaseTest.pageDocument);
+		SpringContextLoaderBaseTest.tester
+				.assertComponentOnAjaxResponse("graveyardParent:graveyard:graveyardCardsPlaceholder:graveyardCards:0:wrapper:graveyardImagePlaceholder");
 
 		// Verify
 		allCardsInBattlefield = persistenceService.getAllCardsInBattleFieldForAGame(gameId);
