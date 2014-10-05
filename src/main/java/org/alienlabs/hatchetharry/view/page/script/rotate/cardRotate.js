388ab843abc2062971f1f7626b4be823cab01a4a
nostromo@eclipse <goupilpierre@gmail.com>
2014-10-05 20:35:05 +0200

    Reverted to old wicket-atmosphere version because of a regression

--- src/main/java/org/alienlabs/hatchetharry/view/page/script/rotate/cardRotate.js
+++ src/main/java/org/alienlabs/hatchetharry/view/page/script/rotate/cardRotate.js
@@ -1,7 +1,9 @@
-window.setTimeout(function () {
-	jQuery("#cardRotate${uuidValidForJs}").data("tapUrl", "${url}");
+jQuery(function() { 
+	window.setTimeout(function () {
+		jQuery("#cardRotate${uuidValidForJs}").data("tapUrl", "${url}");
 
-    jQuery('#cardRotate${uuidValidForJs}').unbind('click');
-    var tapUrl${uuidValidForJs} = jQuery('#cardRotate${uuidValidForJs}').data('tapUrl');
-    Wicket.Ajax.get({'u': tapUrl${uuidValidForJs}, 'e' : 'click', 'c' : 'cardRotate${uuidValidForJs}'});
-}, 500);
+    	jQuery('#cardRotate${uuidValidForJs}').unbind('click');
+    	var tapUrl${uuidValidForJs} = jQuery('#cardRotate${uuidValidForJs}').data('tapUrl');
+    	Wicket.Ajax.get({'u': tapUrl${uuidValidForJs}, 'e' : 'click', 'c' : 'cardRotate${uuidValidForJs}'});
+	}, 2000);
+});
