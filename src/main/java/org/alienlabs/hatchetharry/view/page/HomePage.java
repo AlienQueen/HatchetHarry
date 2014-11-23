25bb73e2d8befbfe4ad1ba915d7ebba0e3ac8323
AlienQueen@nostromo <goupilpierre@gmail.com>
2014-11-23 20:40:41 +0100

    Updated pom

--- src/main/java/org/alienlabs/hatchetharry/view/page/HomePage.java
+++ src/main/java/org/alienlabs/hatchetharry/view/page/HomePage.java
@@ -379,7 +379,7 @@ public class HomePage extends TestReportPage
 
 		// Welcome message
 		final Label message1 = new Label("message1", "version 0.23.0 (release Battlefield),");
-		final Label message2 = new Label("message2", "built on Sunday, 16th of November 2014.");
+		final Label message2 = new Label("message2", "built on Sunday, 23th of November 2014.");
 		this.add(message1, message2);
 
 		// Comet clock channel
